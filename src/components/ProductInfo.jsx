1ecb6cfa0247b28af7e2deee6bc4336780b23714
unknown <jules.rubin@efrei.net>
2023-10-01 18:20:52 +0200

    added modal for VTO

--- src/components/ProductInfo.jsx
+++ src/components/ProductInfo.jsx
@@ -1,4 +1,4 @@
-import { VtmnPrice, VtmnRating, VtmnButton, VtmnIcon } from '@vtmn/react';
+import { VtmnPrice, VtmnRating, VtmnButton, VtmnIcon, VtmnModal, VtmnModalTitle, VtmnModalDescription, VtmnModalActions, VtmnTooltip } from '@vtmn/react';
 import { IoIosArrowDown, IoIosArrowUp } from 'react-icons/io';
 import { useState } from 'react';
 
@@ -6,6 +6,7 @@ const ProductInfo = () => {
     const sizeOptions = ['XS', 'S', 'M', 'L', 'XL', '2XL', '3XL']
     const [selectedSize, setSelectedSize] = useState(null)
     const [isDropdownVisible, setIsDropdownVisible] = useState(false);
+    const [open, setOpen] = useState(false);
 
     const selectSize = (size) => {
         setSelectedSize(size)
@@ -17,7 +18,7 @@ const ProductInfo = () => {
     };
 
     return (
-        <div className="text-left px-5 pt-8 flex flex-col pb-5">
+        <div className="text-left px-5 pt-4 flex flex-col pb-5">
             <p className="text-[#001018] text-[1.6rem] font-bold">T-shirt Fitness Femme - 500 <br></br>Essentials noir</p>
             <div className="flex pt-2">
                 <VtmnPrice
@@ -38,7 +39,6 @@ const ProductInfo = () => {
                     className=""
                     comments=""
                     name="rating"
-                    onChange={function noRefCheck(){}}
                     readonly
                     showValue
                     size="small"
@@ -48,12 +48,11 @@ const ProductInfo = () => {
             </div>
             {/* font is roboto condensed */}
             <p className="font-condensed text-[#001018] text-[0.875rem] leading-[1.43] pt-2">
-                Éco-conçu avec 30% de coton recyclé ce tee-shirt, 190g/m², de fitness femme à manches courtes, se porte à la salle de sport, comme en ville !
-                <br></br>
-                <br></br>
+                Éco-conçu avec 30% de coton recyclé ce tee-shirt, 190g/m², de fitness femme à manches courtes, se porte à la salle de sport, comme en ville !</p>
+            <p className="font-condensed text-[#001018] text-[0.875rem] leading-[1.43] pt-2">
                 Pièce maitresse de notre collection ESSENTIEL, ce tee-shirt femme intemporel est conçu pour le sport comme pour la vie quotidienne. Notre choix de matière contribue à la durabilité du produit.
             </p>
-            <h2 className='font-condensed italic text-[#001018] text-[0.9rem] leading-[1.23] font-bold pt-8'>TAILLE :</h2>
+            <h2 className='font-condensed italic text-[#001018] text-[0.9rem] leading-[1.23] font-bold pt-4'>TAILLE :</h2>
             <div className="dropdown pt-2">
                 <label
                     tabIndex={0}
@@ -85,6 +84,50 @@ const ProductInfo = () => {
                     ))}
                     </ul>
                 )}
+            </div>
+            <div className='flex flex-row items-center justify-center py-2 w-full'>
+                <VtmnTooltip
+                    position='bottom'
+                    tooltip='Sélectionnez une taille pour activer le VTO'
+                    style={{
+                        visibility: selectedSize ? 'hidden' : 'visible',
+                    }}>
+                    <VtmnButton
+                        {...(selectedSize ? {} : { disabled: true })}
+                        style={{
+                            visibility: 'visible',
+                            fontFamily: 'Roboto Condensed',
+                            fontWeight: '700',
+                            fontSize: '0.8rem',
+                            lineHeight: '1.5rem',
+                            borderRadius: "0.15rem",
+                        }}
+
+                        size='small'
+                        variant='primary'
+                        onClick={()=> setOpen(true)}>
+                            VIRTUAL TRY ON
+                    </VtmnButton>
+                </VtmnTooltip>
+                <VtmnModal
+                    open={open}
+                    onClose={() => setOpen(false)}
+                >
+                    <VtmnModalTitle>
+                        This is the title
+                    </VtmnModalTitle>
+                    <VtmnModalDescription>
+                        Chosen size is {selectedSize}
+                    </VtmnModalDescription>
+                    <VtmnModalActions>
+                        <VtmnButton
+                            onClick={() => setOpen(false) }
+                            variant="primary"
+                        >
+                            OK
+                        </VtmnButton>
+                    </VtmnModalActions>
+                </VtmnModal>
             </div>
             <p className='pt-2 font-condensed font-semibold text-[0.9rem]'>Livraison à domicile - sous 3 à 4 jours</p>
             <div className='flex flex-row items-center'>
